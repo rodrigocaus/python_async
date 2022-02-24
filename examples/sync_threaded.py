1d7bc42fb7c689e0dfcb30cfe45b0914e189d92c
rodrigocaus <digocaus@hotmail.com>
2022-02-24 16:56:04 -0300

    fix(sync_threaded): urllib3 requires to increase pool size in order to use multithreading

--- examples/sync_threaded.py
+++ examples/sync_threaded.py
@@ -4,13 +4,17 @@ from tasks import io
 from util import timecount
 
 
+MAX_CONNECTIONS = 100
+
 @timecount.exectime
 def call():
+    adapter = requests.adapters.HTTPAdapter(pool_connections=MAX_CONNECTIONS, pool_maxsize=MAX_CONNECTIONS)
     print('Index,Exists')
     page = 'https://en.wikipedia.org/wiki/{}'
     args = []
-    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_CONNECTIONS) as executor:
         with requests.Session() as session:
+            session.mount('http://', adapter)
             for i in range(100):
                 args.append((page.format(i), session))
 
